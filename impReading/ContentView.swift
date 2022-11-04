021742d93bb8cf07f5bbe4e23eac3ccd4b0826da
Byeong Rok <rokany@cau.ac.kr>
2022-11-04 18:02:29 +0900

    one

---
+++ impReading/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  impReading
+//
+//  Created by Rocket on 2022/11/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("실천독서")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ impReading/impReadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  impReadingApp.swift
+//  impReading
+//
+//  Created by Rocket on 2022/11/04.
+//
+
+import SwiftUI
+
+@main
+struct impReadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
